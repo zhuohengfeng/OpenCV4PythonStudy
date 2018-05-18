2afcaeefbb6ca6249ab0c44b0bc89c76bc8f5634
zhuohf1 <zhuohf1@lenovo.com>
2018-05-18 11:35:08 +0800

    通过随机数生成灰度&彩色图片

--- Chapter01.py
+++ Chapter01.py
@@ -11,4 +11,20 @@
 
 import cv2
 import numpy as np
+import os
 
+
+''' ========例子1. 生产2张随机图片====== '''
+# 生产120000个字节的随机数组
+radomByteArray = bytearray(os.urandom(120000))
+# 转化成numpy数组
+flatNumpyArray = np.array(radomByteArray)
+# 转化成400*300的灰度图片,一个像素一个字节
+grayImage = flatNumpyArray.reshape(300,400)
+cv2.imwrite('RandomGray.png', grayImage)
+# 转化成400*100的有颜色的图片，一个像素需要3个字节
+bgrImage = flatNumpyArray.reshape(100, 400, 3)
+cv2.imwrite('RandomColor.png', bgrImage)
+
+
+''' ========例子2. 生产2张随机图片====== '''
